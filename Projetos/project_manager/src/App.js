28c6c3e73e9a66d2df4b8b240276f155af900214
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-08-11 20:39:18 +0100

    estudo

--- Projetos/project_manager/src/App.js
+++ Projetos/project_manager/src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List'
+// import HelloWorld from './components/HelloWorld';
+// import SayMyName from './components/SayMyName';
+// import Pessoa from './components/Pessoa';
+// import List from './components/List'
+import Form from './components/Form'
+import Evento from './components/Evento'
 
 function App() {
-  const name = 'Renan'
+  //   const name = 'Renan'
 
-  function sum(a, b) {
-    return a + b
-  }
+  //   function sum(a, b) {
+  //     return a + b
+  //   }
 
-  const url = 'https://via.placeholder.com/150'
+  //   const url = 'https://via.placeholder.com/150'
 
   return (
     <div className="App">
-      <p>Soma: {sum(1, 2)}</p>
+      <Evento numero={1}/>
+      <Evento numero={2}/>
+      <Form />
+      {/* <p>Soma: {sum(1, 2)}</p>
       <img src={url} alt="Minha imagem" />
       <HelloWorld />
       <SayMyName nome={name} />
@@ -25,7 +30,7 @@ function App() {
         profissao="Programador"
         foto="https://via.placeholder.com/150"
       />
-      <List/>
+      <List/> */}
     </div>
   );
 }
