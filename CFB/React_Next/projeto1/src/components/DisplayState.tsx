09169d19c171d84e5cad28dc8b65135e6835cfc1
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-02-16 20:26:50 +0000

    estudo

--- CFB/React_Next/projeto1/src/components/DisplayState.tsx
+++ CFB/React_Next/projeto1/src/components/DisplayState.tsx
@@ -1,9 +1,25 @@
 interface DisplayStateProps{
   valor:number,
+  fvalor: any,
+  // fadicionar: any,
+  // fsubtrair: any,
 }
 
 export default function DisplayState(props:DisplayStateProps){
+
+  function operacao (op:number){
+    let c = props.valor
+    c+= op
+    props.fvalor(c)
+  }
+
   return (
-    <div>{props.valor}</div>
+    <div className="flex flex-col p-5 rounded-md bg-zinc-300 justify-center items-center w-[300px]">
+      <div className="flex bg-zinc-700 text-green-500 justify-center items-center w-[200px] mb-3">{props.valor}</div>
+      <div className="flex gap-5">
+        <button onClick={() => operacao(1)} className="btnPadrao">adicionar</button>
+        <button onClick={() => operacao(-1)} className="btnPadrao">subtrair</button> {/*Sempre que for passar parâmetros, usa-se arrow function */}
+      </div>
+    </div>
   )
 }
--- CFB/React_Next/projeto1/src/pages/usestate/usestate.tsx
+++ CFB/React_Next/projeto1/src/pages/usestate/usestate.tsx
@@ -1,28 +1,17 @@
 import { useState } from "react";
 import DisplayState from "@/components/DisplayState";
 
+// const usestate = () => {
+
 export default function usestate() {
   const [cont, setCont] = useState<number>(10);
 
-  function adicionar (){
-    let c = cont
-    c++
-    setCont(c)
-  }
-
-  function subtrair (){
-    let c = cont
-    c--
-    setCont(c)
-  }
   return (
     <div>
       useState
-      <DisplayState valor={cont}></DisplayState>
-      <div className="flex gap-5">
-        <button onClick={adicionar} className="btnPadrao">adicionar</button>
-        <button onClick={subtrair} className="btnPadrao">subtrair</button>
-      </div>
+      <DisplayState valor={cont} fvalor={setCont}></DisplayState>
     </div>
   );
 }
+
+// export default usestate
