09fb724f413dae63891ad6a149d0f44381cb2d38
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-03-06 21:11:28 +0000

    estudo

--- Alura/1 - React - Desenvolvendo com Javascript/organo/src/componentes/CampoTexto/index.js
+++ Alura/1 - React - Desenvolvendo com Javascript/organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,16 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value)
+    // console.log(evento.target.value)
+  }
+
   return (
     <fieldset className="campo-texto"> 
       <label >{props.label}</label>
-      <input required={props.obrigatorio} placeholder={props.placeholder}/>
+      <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
     </fieldset>
   )
 }
