7d78b594f2e5ece744ea44dcebf501563ab1bdba
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-03-17 23:30:11 +0000

    estudo

---
+++ Alura/React JS/4 - React - Estilize componentes com Styled Components e manipule arquivos estáticos/space-app/src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+const ItemListaEstilizado = styled.li`
+  margin-bottom: 30px;
+  font-size: 24px;
+  line-height: 29px;
+  cursor: pointer;
+  color: ${ props => props.$ativo ? '#7b78e5' : '#d9d9d9' };
+  font-family: ${ props => props.$ativo ? 'GandhiSansBold' : 'GandhiSansRegular' };
+  display: flex;
+  align-items: center;
+  gap: 22px;
+
+`
+
+const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
+  return (
+    <ItemListaEstilizado $ativo={ativo}>
+      <img src={ativo? iconeAtivo : iconeInativo} alt="" />
+      {children}
+    </ItemListaEstilizado>
+  )
+}
+
+export default ItemNavegacao
---
+++ Alura/React JS/4 - React - Estilize componentes com Styled Components e manipule arquivos estáticos/space-app/src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components"
+import ItemNavegacao from "./ItemNavegacao"
+
+const ListaEstilizada = styled.ul`
+  width: 212px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`
+
+const BarraLateral = () => {
+  return (
+    <aside>
+      <nav>
+        <ListaEstilizada>
+          <ItemNavegacao 
+            iconeAtivo="/icones/home-ativo.png"
+            iconeInativo="/icones/home-inativo.png"  
+            ativo
+          >
+            Início
+          </ItemNavegacao>
+          <ItemNavegacao 
+            iconeAtivo="/icones/mais-vistas-ativo.png"
+            iconeInativo="/icones/mais-vistas-inativo.png"  
+            // ativo
+          >
+            Mais vistas
+          </ItemNavegacao>
+        </ListaEstilizada>
+      </nav>
+    </aside>
+  )
+}
+
+export default BarraLateral
