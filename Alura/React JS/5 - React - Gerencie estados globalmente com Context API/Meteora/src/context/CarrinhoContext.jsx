669650865eec7ab2385aadc491ad9e554abbd04a
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-04-10 21:13:31 +0100

    aula 4 - estudo

--- Alura/React JS/5 - React - Gerencie estados globalmente com Context API/Meteora/src/context/CarrinhoContext.jsx
+++ Alura/React JS/5 - React - Gerencie estados globalmente com Context API/Meteora/src/context/CarrinhoContext.jsx
@@ -2,16 +2,25 @@ import { useState } from "react";
 import { createContext } from "react";
 
 export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = "Carrinho";
 
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, setCarrinho] = useState([])
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
 
-
-  return(
-    <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidade,
+        setQuantidade,
+        valorTotal,
+        setValorTotal,
+      }}
+    >
       {children}
     </CarrinhoContext.Provider>
-  )
-}
-
-
+  );
+};
