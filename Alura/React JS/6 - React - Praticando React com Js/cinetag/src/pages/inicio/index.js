f15b8350b93827d2e7860d26a9321f3a1bd51cdd
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-04-22 22:21:50 +0100

    estudo - aula 1
    preparação do ambiente, configurações, cabeçalho e rodapé

---
+++ Alura/React JS/6 - React - Praticando React com Js/cinetag/src/pages/inicio/index.js
@@ -0,0 +1,13 @@
+import Cabecalho from "components/cabecalho";
+import Rodape from "components/rodape";
+
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  )
+}
+
+export default Inicio;
