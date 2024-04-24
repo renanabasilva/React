77f1fa7a008f608a722c57b2926e94460a9c704d
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-04-25 00:01:52 +0100

    aula 02 - Estudo

--- Alura/React JS/6 - React - Praticando React com Js/cinetag/src/routes.js
+++ Alura/React JS/6 - React - Praticando React com Js/cinetag/src/routes.js
@@ -1,12 +1,21 @@
+import Cabecalho from "components/cabecalho";
+import Container from "components/container";
+import Rodape from "components/rodape";
+import Favoritos from "pages/favoritos";
 import Inicio from "pages/inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />}></Route>
-      </Routes>
+      <Cabecalho />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />}></Route>
+          <Route path="/favoritos" element={<Favoritos />}></Route>
+        </Routes>
+      </Container>
+      <Rodape />
     </BrowserRouter>
   )
 }
