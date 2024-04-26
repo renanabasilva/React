d77af102209853a53a6183bc6a8f8b1e8a5194f7
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-04-26 19:46:46 +0100

    aula 04 - estudo

--- Alura/React JS/6 - React - Praticando React com Js/cinetag/src/components/card/index.js
+++ Alura/React JS/6 - React - Praticando React com Js/cinetag/src/components/card/index.js
@@ -1,4 +1,5 @@
 import { useFavoritoContext } from "contextos/favoritos";
+import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png"
 import iconeDesfavoritar from "./desfavoritar.png"
@@ -9,8 +10,10 @@ function Card({ id, titulo, capa }) {
   const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
   return (
     <div className={styles.container}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
+      <Link className={styles.link} to={`/${id}`}>
+        <img src={capa} alt={titulo} className={styles.capa} />
+        <h2>{titulo}</h2>
+      </Link>
       <img
         src={icone}
         alt={"Favoritar filme"}
---
+++ Alura/React JS/6 - React - Praticando React com Js/cinetag/src/pages/player/index.js
@@ -0,0 +1,40 @@
+import { useParams } from "react-router-dom";
+import styles from "./Player.module.css";
+import videos from "json/db.json";
+import Banner from "components/banner";
+import Titulo from "components/titulo";
+import NaoEncontrada from "pages/naoEncontrada";
+
+function Player() {
+  const parametros = useParams();
+  const video = videos.find((video) => {
+    return video.id === Number(parametros.id);
+  })
+
+  if (!video)
+    return <NaoEncontrada />
+  
+  return (
+    <>
+      <Banner imagem="player" />
+      <Titulo>
+        <h1>Player</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <iframe
+          width="100%"
+          height="100%"
+          src={video.link}
+          title={video.titulo}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        >
+        </iframe>
+      </section>
+    </>
+  )
+}
+
+export default Player;
