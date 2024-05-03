efe0f36a15be286fba8b4890a7c01db2cad072cb
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-05-03 18:25:46 +0100

    aula 02 - Botao e CampoTexto

--- Alura/React JS/7 - React - Migrando para Typescript/organo/src/componentes/Botao/index.tsx
+++ Alura/React JS/7 - React - Migrando para Typescript/organo/src/componentes/Botao/index.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { ReactElement } from "react"
+import React, { ReactElement } from "react"
+// import { ReactElement } from "react"
 import "./Botao.css"
 
 interface BotaoProps {
