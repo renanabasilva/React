954c916eacf1676a568b16ac8a44e977204c2c22
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-12-16 13:35:15 +0000

    aula 06 - estudo

--- Alura/React TS/2 - React - Conhecendo a biblioteca React Router/aluroni/src/pages/Prato/index.tsx
+++ Alura/React TS/2 - React - Conhecendo a biblioteca React Router/aluroni/src/pages/Prato/index.tsx
@@ -1,20 +1,22 @@
 import styles from './Prato.module.scss';
 // import { useLocation } from 'react-router-dom';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   // const { state } = useLocation();
   // const { prato } = state as { prato: typeof cardapio[0]};
   const navigate = useNavigate();
   const { id } = useParams();
-  const prato = cardapio.find(item => item.id === Number(id));
+  const prato = cardapio.find((item) => item.id === Number(id));
   if (!prato){
-    return '';
+    return <NotFound />;
   }
   return(
-    <>
+    <PaginaPadrao>
       <button 
         className={styles.voltar}
         onClick={() => navigate(-1)}
@@ -35,6 +37,39 @@ export default function Prato() {
           <TagsPrato {...prato}/>
         </div>
       </section>
-    </>
+    </PaginaPadrao>
   );
+  // return (
+  //   <Routes>
+  //     <Route path='*' element={<PaginaPadrao />}>
+  //       <Route 
+  //         index 
+  //         element={
+  //           <>
+  //             <button 
+  //               className={styles.voltar}
+  //               onClick={() => navigate(-1)}
+  //             >
+  //               {'< Voltar'}
+  //             </button>
+  //             <section className={styles.container}>
+  //               <h1 className={styles.titulo}>
+  //                 {prato.title}
+  //               </h1>
+  //               <div className={styles.imagem}>
+  //                 <img src={prato.photo} alt={prato.title} />
+  //               </div>
+  //               <div className={styles.conteudo}>
+  //                 <p className={styles.conteudo__descricao}>
+  //                   {prato.description}
+  //                 </p>
+  //                 <TagsPrato {...prato}/>
+  //               </div>
+  //             </section>
+  //           </>
+  //         } 
+  //       />
+  //     </Route>
+  //   </Routes>
+  // );
 }
