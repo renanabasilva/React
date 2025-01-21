2643222e7b0656ba3f0f428bb921f9685be72783
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2025-01-21 20:31:20 +0000

    aula 04 - estudo

--- Alura/React TS/3 - React - Integrando seu projeto React com APIs/alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ Alura/React TS/3 - React - Integrando seu projeto React com APIs/alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,50 +1,64 @@
-import { Button, TextField } from "@mui/material";
-import axios from "axios";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
+import http from "../../../http";
 
 const FormularioRestaurante = () => {
-
   const parametros = useParams();
 
   useEffect(() => {
     if (parametros.id) {
-      axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
-        .then(resposta => setNomeRestaurante(resposta.data.nome))
+      http
+        .get<IRestaurante>(
+          `restaurantes/${parametros.id}/`
+        )
+        .then((resposta) => setNomeRestaurante(resposta.data.nome));
     }
-  },[parametros])
+  }, [parametros]);
 
-  const [nomeRestaurante, setNomeRestaurante] = useState<string>('');
+  const [nomeRestaurante, setNomeRestaurante] = useState<string>("");
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
 
-    if(parametros.id){
-      axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
-        nome: nomeRestaurante
-      })
-        .then(() => alert('Restaurante alterado com sucesso!'))
-    } else{
-      axios.post('http://localhost:8000/api/v2/restaurantes/', {
-        nome: nomeRestaurante
-      })
-        .then(() => alert('Restaurante cadastrado com sucesso!'))
+    if (parametros.id) {
+      http
+        .put(`restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then(() => alert("Restaurante alterado com sucesso!"));
+    } else {
+      http
+        .post("restaurantes/", {
+          nome: nomeRestaurante,
+        })
+        .then(() => alert("Restaurante cadastrado com sucesso!"));
     }
+  };
 
-  }
-
-  return(
-    <form onSubmit={aoSubmeterForm}>
-      <TextField 
-        value={nomeRestaurante} 
-        onChange={evento => setNomeRestaurante(evento.target.value)} 
-        label="Nome do Restaurante" 
-        variant="standard" 
-      />
-      <Button type="submit" variant="outlined">Salvar</Button>
-    </form>
-  )
-}
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setNomeRestaurante(evento.target.value)}
+          label="Nome do Restaurante"
+          variant="standard"
+          fullWidth
+          required
+        />
+        <Button sx={{marginTop:1}} type="submit" fullWidth variant="outlined">
+          Salvar
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
 export default FormularioRestaurante;
